747b13cba23a1b16daf8976889aae9044cc06662
kal30 <rkalyani@gmail.com>
2024-10-08 11:41:39 -0500

    moved files around

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth"; // Firebase auth
-import AppRoutes from "../routes/Routes"; // Import the Routes file
+import AppRoutes from "./routes/Routes"; // Import the Routes file
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "../theme";
-import Navbar from "./Landing/NavBar"; // Import the Navbar component
-import { auth } from "../services/firebase";
+import theme from "./assets/theme/light";
+import Navbar from "./components/Landing/NavBar"; // Import the Navbar component
+import { auth } from "./services/firebase";
 
 const App = () => {
   const [user, setUser] = useState(null);
