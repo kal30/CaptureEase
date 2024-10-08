87799c51fb0c5f1faf1ebe659275ef95ab1c7419
kal30 <rkalyani@gmail.com>
2024-10-07 19:29:17 -0500

    Cleaned up login and register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,51 +1,11 @@
-// src/pages/Register.js
-import React, { useState } from 'react';
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { auth } from '../services/firebase';
-import { Button, Container, Typography, Box } from '@mui/material';
-import { useNavigate } from 'react-router-dom';  // For redirection after login
+import React from "react";
+import GoogleAuth from "../components/Register/GoogleAuth"; // Import the reusable component
 
 const Register = () => {
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();  // Initialize navigate for redirection
-
-  // Google sign-in handler
-  const handleGoogleSignIn = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      await signInWithPopup(auth, provider);
-      navigate('/dashboard');  // Redirect to dashboard after successful Google Sign-In
-    } catch (error) {
-      setError('Failed to sign in with Google.');
-      console.error(error);
-    }
-  };
-
   return (
-    <Container maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Sign in with Google
-        </Typography>
-        {error && <Typography color="error">{error}</Typography>}
-        <Button
-          fullWidth
-          variant="outlined"
-          color="secondary"
-          onClick={handleGoogleSignIn}
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Sign in with Google
-        </Button>
-      </Box>
-    </Container>
+    <GoogleAuth
+      buttonText="Register with Google" // Custom button text for register
+    />
   );
 };
 
