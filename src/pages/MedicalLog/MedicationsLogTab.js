b53608d953ef865bcd796f4200cad00e7b7a7586
Kalyani Rajamani <Kalyani.Rajamani@archesys.io>
2025-07-14 12:46:26 -0500

    feat: Widen side effect modal and adjust padding
    - Increased the width of the 'Add Side Effect' modal to 800px to provide more space for fields.
    - Reduced internal padding of the side effect modal to optimize horizontal space for input fields.

--- src/pages/MedicalLog/MedicationsLogTab.js
+++ src/pages/MedicalLog/MedicationsLogTab.js
@@ -17,11 +17,11 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 600,
+  width: 800,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
-  p: 4,
+  p: 2,
 };
 
 const MedicationsLogTab = ({ childId }) => {
